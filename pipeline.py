e48b106bdc399d196b2f349a09d57d58e3c257c9
Lala Aliyeva <lalaamin027@gmail.com>
2026-09-11 20:22:07 +0200

    Creating response text file

--- pipeline.py
+++ pipeline.py
@@ -5,18 +5,21 @@ url = "https://api.github.com/repos/mitre-attack/attack-stix-data"
 response = requests.get(url)
 print(response.status_code)
 
-data = response.json()
+
 #print(data.keys())
 
 #print(json.dumps(data,indent = 4))
 
 if response.status_code == 200:
+    data = response.json()
     result = {
     "repository": data["name"],
     "description": data["description"],
     "stars": data["stargazers_count"],
     "open_issues": data["open_issues_count"]
 }
-    print(result)
+    #print(result)
+    with open("result.txt","w") as file:
+        json.dump(result,file,indent = 4)
 else:
-    print("else srabotal")
+    print("Request failed:", response.status_code)
